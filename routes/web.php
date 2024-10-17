5181a2fe73b62813c1d8f1eb2d5427490675a708
Murilo Melo <murilomelo94@outlook.com>
2024-10-17 18:07:51 -0300

    build: :building_construction: change '/' response

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return [
+        'Inovanti-API' => 'v1.0.0',
+        'Laravel' => app()->version()
+    ];
 });
