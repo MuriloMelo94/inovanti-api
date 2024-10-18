6ae63660e7450f493bc487551166ebd26e49dc55
Murilo Melo <murilomelo94@outlook.com>
2024-10-18 15:10:00 -0300

    test: :test_tube: add logout test

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -37,3 +37,23 @@ test('Wrong credentials should not be able to login', function () {
 
     $response->assertStatus(401);
 });
+
+test('Admin user should be able to logout', function () {
+    User::factory()->create([
+        'name' => 'Admin',
+        'username' => 'admin',
+        'email' => 'admin@email.com',
+        'password' => bcrypt('112233'),
+    ]);
+
+    $credentials = [
+        'username' => 'admin',
+        'password' => '112233',
+    ];
+
+    $response = $this->post('api/login', $credentials);
+
+    $token = $response->json('access_token');
+
+    $this->post('api/logout', [], ['Authorization' => 'Bearer ' . $token])->assertStatus(200);
+});
