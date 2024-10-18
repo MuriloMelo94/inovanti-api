09e0a173baf7c4a9875ef6a408a7eab3a259c7eb
Murilo Melo <murilomelo94@outlook.com>
2024-10-18 14:46:15 -0300

    test: :white_check_mark: update tests to run in sqlite environment

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -1,23 +1,39 @@
 <?php
 
+use App\Models\User;
+
 test('Admin user should be able to login', function () {
+    User::factory()->create([
+        'name' => 'Admin',
+        'username' => 'admin',
+        'email' => 'admin@email.com',
+        'password' => bcrypt('112233'),
+    ]);
+
     $credentials = [
         'username' => 'admin',
         'password' => '112233',
     ];
 
-    $response = $this->post('/login', $credentials);
+    $response = $this->post('api/login', $credentials);
 
     $response->assertStatus(200);
 });
 
 test('Wrong credentials should not be able to login', function () {
+    User::factory()->create([
+        'name' => 'Admin',
+        'username' => 'admin',
+        'email' => 'admin@email.com',
+        'password' => bcrypt('112233'),
+    ]);
+
     $credentials = [
         'username' => 'admin',
         'password' => 'wrong-password',
     ];
 
-    $response = $this->post('/login', $credentials);
+    $response = $this->post('api/login', $credentials);
 
     $response->assertStatus(401);
 });
